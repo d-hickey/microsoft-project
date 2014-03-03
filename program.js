4b6ce67e5e81e550e045cb3d9db47ed75817d618
LianaT <tomescul@tcd.ie>
2014-03-03 22:50:35 +0000

    Create program.js
    This is the javascript file

---
+++ program.js
@@ -0,0 +1,30 @@
+// The initialize function is required for all apps.
+Office.initialize = function (reason) {
+    // Checks for the DOM to load using the jQuery ready function.
+    $(document).ready(function () {
+    // After the DOM is loaded, app-specific code can run.
+    // Add any initialization logic to this function.
+    });
+}
+var MyArray = [['Berlin'],['Munich'],['Duisburg']];
+function writeData() {
+    Office.context.document.setSelectedDataAsync(MyArray, { coercionType: 'text' });
+}
+
+function test() {
+	document.getElementById("test").innerText = "yohoho: "+document.getElementById("results").innerText;
+}
+
+function ReadData() {
+    Office.context.document.getSelectedDataAsync("text", function (result) {
+        if (result.status === "succeeded"){
+			document.getElementById("results").innerText = result.value;
+        }
+		/*
+        else{
+            printData(result.error.name + ":" + err.message);
+        }*/
+    });
+	prettyPrint();
+}
+	  
