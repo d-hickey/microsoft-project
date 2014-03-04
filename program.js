5b1da65ff95d880c74ea24ea17fbd8165188eb5d
d-hickey <hickeyd1@tcd.ie>
2014-03-04 12:56:13 +0000

    Added basic parser for Java, changed xml details
    will add the xml file to ignore after this as the source location is
    different for everyone

--- program.js
+++ program.js
@@ -6,9 +6,94 @@ Office.initialize = function (reason) {
     // Add any initialization logic to this function.
     });
 }
-var MyArray = [['Berlin'],['Munich'],['Duisburg']];
-function writeData() {
-    Office.context.document.setSelectedDataAsync(MyArray, { coercionType: 'text' });
+
+
+function parseCode(code) {
+	//document.getElementById("debug").outerHTML = "gets into function";
+    var result = "";
+    var pos = 0;
+    var c;
+    var indent = 0;
+	var newline = 0;
+	var infor = 0;
+    code = removeExtraSpaces(code);
+	
+    while(pos < code.length) {
+        c = code.charAt(pos);
+        if (infor === 0){
+			if (c === '}'){
+				indent = indent - 1;
+			}
+			if(newline === 1){
+				result = result + makeIndent(indent);
+				newline = 0;
+			}
+			
+			result = result + c;
+		
+        
+			if (c === '{'){
+				indent = indent + 1;
+				while(code.charAt(pos+1) === ' '){
+					pos = pos + 1;
+				}
+				result = result + '\n';
+				newline = 1;
+			}
+			if (c === ';'){
+				while(code.charAt(pos+1) === ' '){
+					pos = pos + 1;
+				}
+				result = result + '\n';
+				newline = 1;
+			}
+			// this part doesn't seem to work too well atm
+			/*if (code.substring(pos, pos+3) == "for("){
+				infor = 1;
+			}*/
+			
+			
+		}
+		else{
+			result = result + c;
+			if(code.charAt(pos+1) === '{'){
+				infor = 0;
+			}
+		}
+  
+        pos = pos + 1;
+    }
+	//document.getElementById("debug").outerHTML = "finishes parse function";
+    return result;
+}
+
+function removeExtraSpaces(code){
+    var result = "";
+    var seenSpace = 0;
+    var c;
+    for(var i = 0; i < code.length; i++){
+        c = code.charAt(i);
+        if(c !== '\n'){
+            if(c !== ' ' || seenSpace != 1){
+				seenSpace = 0;
+                result = result + c;
+                if(c === ' '){
+                    seenSpace = 1;
+                }
+            }
+        }
+    }
+    return result;
+}
+
+function makeIndent(ind) {
+    var space = "";
+    var i = 0;
+    while(i < ind){
+        space = space + "    ";
+        i++;
+    }
+    return space;
 }
 
 function test() {
@@ -16,15 +101,22 @@ function test() {
 }
 
 function ReadData() {
-    Office.context.document.getSelectedDataAsync("text", function (result) {
+	var code = document.getElementById('code').value;
+	
+   /* parseCode(code, function (result) {
         if (result.status === "succeeded"){
 			document.getElementById("results").outerHTML = "<code id=\"results\" class=\"prettyprint\">" + result.value + "</code>";
+			Office.context.document.setSelectedDataAsync(result.value, { coercionType: 'text' });
         }
-		/*
         else{
             printData(result.error.name + ":" + err.message);
-        }*/
-    });
+        }
+    });*/
+	document.getElementById("debug").outerHTML = "read code: "  + code;
+	var formatted = parseCode(code);
+	document.getElementById("deb2").outerHTML = "Passed Formatting";
+	document.getElementById("results").outerHTML = "<pre id=\"results\" class=\"prettyprint\">" + formatted + "</pre>";
+	Office.context.document.setSelectedDataAsync(formatted, { coercionType: 'text' });
 	PR.prettyPrint();
 }
 	  
